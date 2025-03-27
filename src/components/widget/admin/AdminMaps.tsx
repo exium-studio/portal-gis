756631950f2c75cdc146b083bb7f99bd8407af5a
sulenq <fatwalinovera@gmail.com>
2025-03-27 22:36:33 +0700

    feat: map style context

--- src/components/widget/admin/AdminMaps.tsx
+++ src/components/widget/admin/AdminMaps.tsx
@@ -1,21 +1,31 @@
-import { useRef, useState } from "react";
-import Map, { MapRef } from "react-map-gl/mapbox";
-import mapboxgl from "mapbox-gl";
 import { useColorMode } from "@/components/ui/color-mode";
+import useMapStyle from "@/context/useMapStyle";
+import mapboxgl from "mapbox-gl";
+import { useEffect, useRef, useState } from "react";
+import Map, { MapRef } from "react-map-gl/mapbox";
 
 const AdminMaps = () => {
+  // Contexts
   const { colorMode } = useColorMode();
+  const { mapStyle, setMapStyle } = useMapStyle();
+
+  // States, Refs
   const mapRef = useRef<MapRef>(null);
   const [viewState, setViewState] = useState({
     latitude: 0,
     longitude: 0,
     zoom: 0,
   });
-
   const tiles: Record<string, string> = {
     light: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
     dark: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
   };
+
+  // Handle map style depend on color mode
+  useEffect(() => {
+    if (colorMode === "light") setMapStyle(tiles.light);
+    if (colorMode === "dark") setMapStyle(tiles.dark);
+  }, [colorMode]);
 
   return (
     <Map
@@ -23,7 +33,7 @@ const AdminMaps = () => {
       {...viewState}
       onMove={(evt) => setViewState(evt.viewState)}
       style={{ width: "100%", height: "100vh" }}
-      mapStyle={tiles[colorMode as keyof typeof tiles] ?? tiles.light}
+      mapStyle={mapStyle}
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
       mapLib={mapboxgl}
     />
