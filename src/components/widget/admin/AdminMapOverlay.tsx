8b9f3c8d33cae4b90c3ad80cea6b33d4aa4a82de
sulenq <fatwalinovera@gmail.com>
2025-04-08 10:10:48 +0700

    styling

--- src/components/widget/admin/AdminMapOverlay.tsx
+++ src/components/widget/admin/AdminMapOverlay.tsx
@@ -387,7 +387,16 @@ const DisplayedDataFilter = (props: any) => {
       <DisclosureRoot open={open} lazyLoad size={"xs"}>
         <DisclosureContent>
           <DisclosureHeader>
-            <DisclosureHeaderContent title={`Filter ${pluck(l, item.key)}`} />
+            <DisclosureHeaderContent
+              content={
+                <HStack>
+                  <IconAdjustmentsHorizontal stroke={1.5} size={20} />
+                  <Text fontSize={"16px"} fontWeight={"semibold"}>
+                    {pluck(l, item.key)}
+                  </Text>
+                </HStack>
+              }
+            />
           </DisclosureHeader>
 
           <DisclosureBody>
