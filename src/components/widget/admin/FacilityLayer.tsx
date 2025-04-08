5b48c5f5380644ceee6f7895675a8be84bf171b9
sulenq <fatwalinovera@gmail.com>
2025-04-08 15:27:16 +0700

    styling

--- src/components/widget/admin/FacilityLayer.tsx
+++ src/components/widget/admin/FacilityLayer.tsx
@@ -22,29 +22,8 @@ const FacilityLayer = ({ data }: Props) => {
     })),
   };
 
-  // const size = 0.00005;
-  // const geoJSONData3d: FeatureCollection<Geometry, GeoJsonProperties> = {
-  //   type: "FeatureCollection",
-  //   features: data.map((item) => ({
-  //     type: "Feature",
-  //     properties: { id: item.id, height: 10 },
-  //     geometry: {
-  //       type: "Polygon",
-  //       coordinates: [
-  //         [
-  //           [item.location.lon - size, item.location.lat - size], // left bottom
-  //           [item.location.lon + size, item.location.lat - size], // right bototm
-  //           [item.location.lon + size, item.location.lat + size], // right top
-  //           [item.location.lon - size, item.location.lat + size], // left top
-  //           [item.location.lon - size, item.location.lat - size], // close loop
-  //         ],
-  //       ],
-  //     },
-  //   })),
-  // };
-
   return (
-    <Source id="facility-data-3d" type="geojson" data={geoJSONDataDot}>
+    <Source id="facility-data" type="geojson" data={geoJSONDataDot}>
       <Layer
         id="facility-layer"
         type="circle"
@@ -54,19 +33,6 @@ const FacilityLayer = ({ data }: Props) => {
         }}
       />
     </Source>
-
-    //  <Source id="facility-data-3d" type="geojson" data={geoJSONData3d}>
-    //   <Layer
-    //     id="facility-layer"
-    //     type="fill-extrusion"
-    //     paint={{
-    //       "fill-extrusion-color": ABS_COLORS.orange,
-    //       "fill-extrusion-height": ["get", "height"],
-    //       "fill-extrusion-base": 0,
-    //       "fill-extrusion-opacity": 0.9,
-    //     }}
-    //   />
-    // </Source>
   );
 };
 
