ae2fe8eee7f72704fe8129f271f06c75c56e3e16
sulenq <fatwalinovera@gmail.com>
2025-07-23 22:52:06 +0700

    styling

--- src/pages/admin/MasterDataPage.tsx
+++ src/pages/admin/MasterDataPage.tsx
@@ -1,10 +1,12 @@
 import FeedbackNoData from "@/components/ui-custom/FeedbackNoData";
+import PageContainer from "@/components/widget/PageContainer";
+import { R_GAP } from "@/constants/sizes";
 
 const MasterDataPage = () => {
   return (
-    <>
+    <PageContainer gap={R_GAP} pb={4} flex={1}>
       <FeedbackNoData />
-    </>
+    </PageContainer>
   );
 };
 
