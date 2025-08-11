38ad0f0a55d5c1e3ea03acdea59473f909d48a4c
sulenq <fatwalinovera@gmail.com>
2025-08-11 13:29:05 +0700

    styling

--- src/components/widget/SettingsNavsContainer.tsx
+++ src/components/widget/SettingsNavsContainer.tsx
@@ -87,7 +87,7 @@ const SettingsNavsContainer = ({ children, activePath, ...props }: Props) => {
             overflowY={"auto"}
             maxH={"full"}
           >
-            <CContainer className="scrollY" pl={2} pr={1} gap={4}>
+            <CContainer className="scrollY" px={2} gap={4}>
               {SETTINGS_NAVS.map((item, i) => {
                 return (
                   <CContainer key={i}>
@@ -117,7 +117,7 @@ const SettingsNavsContainer = ({ children, activePath, ...props }: Props) => {
                             {pluck(l, nav.labelKey)}
 
                             {compact && (
-                              <Icon ml={"auto"} mr={-1}>
+                              <Icon ml={"auto"} boxSize={4}>
                                 <IconChevronRight stroke={1.5} />
                               </Icon>
                             )}
@@ -134,7 +134,10 @@ const SettingsNavsContainer = ({ children, activePath, ...props }: Props) => {
       )}
 
       {/* Content */}
-      <PageContainer display={compact && settingsRoute ? "none" : "flex"}>
+      <PageContainer
+        display={compact && settingsRoute ? "none" : "flex"}
+        h={"full"}
+      >
         {children}
       </PageContainer>
     </HStack>
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,6 +1,5 @@
 import ItemContainer from "@/components/ui-custom/ItemContainer";
 import { EmptyState } from "@/components/ui/empty-state";
-import SettingsNavsContainer from "@/components/widget/SettingsNavsContainer";
 import useLang from "@/context/useLang";
 import { Icon } from "@chakra-ui/react";
 import { IconSettings } from "@tabler/icons-react";
@@ -10,20 +9,18 @@ const SettingsPage = () => {
   const { l } = useLang();
 
   return (
-    <SettingsNavsContainer align={"stretch"}>
-      <ItemContainer minH={"full"}>
-        <EmptyState
-          icon={
-            <Icon>
-              <IconSettings />
-            </Icon>
-          }
-          title={l.settings_page.title}
-          description={l.settings_page.description}
-          m={"auto"}
-        />
-      </ItemContainer>
-    </SettingsNavsContainer>
+    <ItemContainer minH={"full"} py={10} flex={1}>
+      <EmptyState
+        icon={
+          <Icon>
+            <IconSettings />
+          </Icon>
+        }
+        title={l.settings_page.title}
+        description={l.settings_page.description}
+        m={"auto"}
+      />
+    </ItemContainer>
   );
 };
 
