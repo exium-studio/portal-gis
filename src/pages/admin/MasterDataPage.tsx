75a31be7661db6f042b8162b1256047b206a5de1
sulenq <fatwalinovera@gmail.com>
2025-08-17 15:32:51 +0700

    styling

--- src/pages/admin/MasterDataPage.tsx
+++ src/pages/admin/MasterDataPage.tsx
@@ -1,6 +1,5 @@
 import ItemContainer from "@/components/ui-custom/ItemContainer";
 import { EmptyState } from "@/components/ui/empty-state";
-import MasterDataNavsContainer from "@/components/widget/MasterDataNavsContainer";
 import useLang from "@/context/useLang";
 import { Icon } from "@chakra-ui/react";
 import { IconSettings } from "@tabler/icons-react";
@@ -10,20 +9,18 @@ const MasterDataPage = () => {
   const { l } = useLang();
 
   return (
-    <MasterDataNavsContainer align={"stretch"}>
-      <ItemContainer minH={"full"}>
-        <EmptyState
-          icon={
-            <Icon>
-              <IconSettings />
-            </Icon>
-          }
-          title={l.settings_page.title}
-          description={l.settings_page.description}
-          m={"auto"}
-        />
-      </ItemContainer>
-    </MasterDataNavsContainer>
+    <ItemContainer minH={"full"} py={8}>
+      <EmptyState
+        icon={
+          <Icon>
+            <IconSettings />
+          </Icon>
+        }
+        title={l.settings_page.title}
+        description={l.settings_page.description}
+        m={"auto"}
+      />
+    </ItemContainer>
   );
 };
 
