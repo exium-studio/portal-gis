2bbd47b601663d3d90b47df42ca9fffb7cc7ed7b
sulenq <fatwalinovera@gmail.com>
2025-08-24 00:04:56 +0700

    styling

--- src/utils/statusBarColor.ts
+++ src/utils/statusBarColor.ts
@@ -1,18 +1,15 @@
-import { useColorMode } from "@/components/ui/color-mode";
-
 export default function useStatusBarColor(
   lightColor: string,
   darkColor: string
 ) {
-  const { colorMode } = useColorMode();
-
   const setStatusBarColor = () => {
+    const html = document.documentElement;
+    const colorScheme = html.style.getPropertyValue("color-scheme");
+    const isDark = colorScheme.trim() === "dark";
+
     const metaThemeColor = document.querySelector('meta[name="theme-color"]');
     if (metaThemeColor) {
-      metaThemeColor.setAttribute(
-        "content",
-        colorMode === "dark" ? darkColor : lightColor
-      );
+      metaThemeColor.setAttribute("content", isDark ? darkColor : lightColor);
     } else {
       console.error('Elemen <meta name="theme-color"> tidak ditemukan.');
     }
