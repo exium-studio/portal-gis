65f7281138361d9ae76a00d3471464c5763da799
sulenq <fatwalinovera@gmail.com>
2025-09-02 19:02:40 +0700

    chore: config making pwa

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        // maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
       },
 
       devOptions: {
