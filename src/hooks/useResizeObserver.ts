e530461a513e6498fc7f7d237cb618dd642a413b
sulenq <fatwalinovera@gmail.com>
2025-09-08 09:27:20 +0700

    refactor

--- src/hooks/useResizeObserver.ts
+++ src/hooks/useResizeObserver.ts
@@ -41,9 +41,9 @@ function useResizeObserver(inputId?: string) {
   return { ref: inputId ? null : ref, dimensions };
 }
 
-function debounce(func: Function, delay: number) {
+function debounce<T extends (...args: any[]) => void>(func: T, delay: number) {
   let timeout: ReturnType<typeof setTimeout>;
-  return function (...args: any[]) {
+  return function (...args: Parameters<T>) {
     clearTimeout(timeout);
     timeout = setTimeout(() => func(...args), delay);
   };
